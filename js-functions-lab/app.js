618113f33edf1e87f106bdd3c0b1a89b503acfa0
realpolya <ps2780@columbia.edu>
2024-08-28 13:23:58 -0400

    Lab is completed

--- js-functions-lab/app.js
+++ js-functions-lab/app.js
@@ -45,6 +45,267 @@ function isAdult(age){
 console.log('Exercise 2 Result:', isAdult(21));
 
 
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+function isCharAVowel(x) {
+  let y = x.toLowerCase();
+  if (y === 'a' || y === 'o' || y === 'e' || y === 'i' || y === 'u') {
+    return(true);
+  } else {
+    return(false);
+  }
+}
+
+console.log('Exercise 3 Result:', isCharAVowel("a"));
 
 
-  
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+function generateEmail(name, domain) {
+  return(`${name}@${domain}`);
+}
+
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
+
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+
+function greetUser(name, time) {
+  return(`Good ${time}, ${name}!`);
+}
+
+console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
+
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+
+function maxOfThree(x, y, z) {
+  if (x > y) {
+    if (x > z) {
+      return x;
+    } else {
+      if (z > y) {
+        return z;
+      }
+    }
+  } else if (x === y) {
+    if (x > z) {
+      return x;
+    } else {
+      return z;
+    }
+  } else {
+    if (y > z) {
+      return y;
+    } else {
+      return z;
+    }
+  }
+}
+
+console.log('Exercise 6 Result:', maxOfThree(28, 29, 32));
+
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+
+function calculateTip(bill, tip) {
+  let tipAmount = bill * (tip / 100);
+  tipAmount = Math.round(tipAmount * 100) / 100; // round to two decimals
+  return tipAmount;
+}
+
+console.log('Exercise 7 Result:', calculateTip(86, 18));
+
+
+
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+
+const convertTemperature = function (temperature, scale) {
+  if (scale === "C") {
+    otherT = (temperature * 9/5) + 32;
+    otherT = Math.round(otherT * 100) / 100; // round to two decimals
+    return otherT;
+  } else if (scale === "F") {
+    otherT = (temperature - 32) * 5/9;
+    otherT = Math.round(otherT * 100) / 100;
+    return otherT;
+  } else {
+    return ("Data not provided correctly");
+  }
+}
+
+console.log('Exercise 8 Result:', convertTemperature(-42, "C"));
+
+
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+
+function basicCalculator(x, y, operation) {
+  if (operation === "add") {
+    return x + y;
+  } else if (operation === "subtract") {
+    return x - y;
+  } else if (operation === "multiply") {
+    return x * y;
+  } else if (operation === "divide") {
+    return x / y;
+  } else {
+    return "Something went wrong";
+  };
+}
+
+console.log('Exercise 9 Result:', basicCalculator(24, 5, "subtract"));
+
+
+
+/*
+Exercise 10: calculateGrade()
+
+Define a function called calculateGrade. 
+It should take a numerical score and return the corresponding letter 
+grade (A, B, C, D, F). 
+
+For example, 90 and above yields an 'A', 80-89 is a 'B', 
+and 70-79 is a 'C', 60-69 is a 'D' and anything lower than a 60 is an 'F'.
+
+Example: calculateGrade(100) should return A.
+
+Complete the exercise in the space below:
+*/
+
+function calculateGrade(score) {
+  if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+
+
+console.log('Exercise 10 Result:', calculateGrade(90));
+
+
+/*
+Exercise 11: createUsername()
+
+Define a function called createUsername. 
+It should take a first name and a last name and return a username. 
+
+The username should be a combination of the following:
+- The first three letters of the first name.
+- The first three letters of the last name.
+- The total character count of the first and last name combined.
+
+Example: createUsername('Samantha', 'Green') should return 'SamGre13'.
+
+Complete the exercise in the space below:
+*/
+
+const createUsername = function (first, last) {
+  let firstName = first.split("");
+  let lastName = last.split("");
+  let username = firstName[0] + firstName[1] + firstName[2];
+  username += lastName[0] + lastName[1] + lastName[2];
+  nameLength = firstName.length + lastName.length;
+  username += nameLength;
+  return username;
+}
+
+console.log('Exercise 11 Result:', createUsername("Polina", "Stepanova"));
+
+
+/*
+Exercise 12: numArgs()
+
+Challenge yourself with numArgs. 
+This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+
+function numArgs(...nums) {
+  return nums.length;
+}
+
+console.log('Exercise 12 Result:', numArgs(1, 4, 5, 6, 7, 9, 100, 43));
+
