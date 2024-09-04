d80b9c85f58b6d631088eefad13d98a55066ea5a
realpolya <ps2780@columbia.edu>
2024-09-04 09:19:54 -0400

    complete up to exercise 10

--- js-objects-lab/app.js
+++ js-objects-lab/app.js
@@ -116,13 +116,54 @@ Hint:
   - Pokemon 7: Squirtle evolves into Pokemon 8: Wartortle
   - Pokemon 25: Pikachu evolves into Pokemon 26: Raichu
 
-More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. When working with an array of objects, the splice() array method is ideal for replacing one element with another. 
+More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. 
+When working with an array of objects, the splice() array method is ideal for 
+replacing one element with another. 
 
 
 Solve Exercise 7 here:
 */
 
+game.party.splice(0,1);
+game.party.unshift(pokemon[1]);
+
+/*
+Exercise 8
+1. Print the name of each Pokémon in your party.
+2. Consider using a loop or an array method to access each Pokémon's name.
+
+Solve Exercise 8 here:
+*/
+
+game.party.forEach((poke) => {
+    console.log(poke.name);
+});
 
 
+/*
+Exercise 9
+1. Can you print out all the starter Pokémon from the `pokemon` array?
+2. Think about how you can identify a starter Pokémon and then log their names.
 
+
+Solve Exercise 9 here:
+*/
+
+// starter poke array is already defined above
+
+starterPoke.forEach((poke) => {
+    console.log(poke);
+});
+
+/*
+Exercise 10
+Create a method called `catchPokemon` and add it to the `game` object. You should not need to edit the original game object directly. This method should:
+  - Accept an object as a parameter called `pokemonObj`
+  - Add the `pokemonObj` to the `game.party` array.
+  - not return anything
+
+After writing this method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+
+Solve Exercise 10 here:
+*/
 
