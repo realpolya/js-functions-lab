40584f6740423c43b2237709cce9c976fe5baa88
realpolya <ps2780@columbia.edu>
2024-09-05 09:38:27 -0400

    calculator is done

--- dom-events-lab/app.js
+++ dom-events-lab/app.js
@@ -3,6 +3,7 @@
 const bodyEl = document.querySelector('body');
 const buttons = document.querySelectorAll('.button');
 const calculator = document.querySelector('#calculator');
+const displayEl = document.querySelector('.display');
 
 /*-------------------------------- Variables --------------------------------*/
 
@@ -10,91 +11,136 @@ const numberOne = [];
 const numberTwo = [];
 const events = [];
 let numberResult = 0;
+let functionToPerform;;
 
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-/*
-buttons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        // function goes here
-        console.log(e.target.innerText);
-    })
-}); */
-
 calculator.addEventListener('click', (e) => {
-    //console.log(e);
 
     // add event to the array
     recordEvents(e.timeStamp, e.target.innerText, e.target.classList);
 
-    // length
+    // length & establish precedeing event
     let lastIndex = events.length - 2;
     let lastEvent = "None";
 
     if (lastIndex < 0) {
-        
         console.log("First event");
-
     } else {
-
         lastEvent = events[lastIndex].class[1];
-
     }
+
+    // cycle through the array to see if the operator was in it
+    let operatorInArray = false;
+    events.forEach((eventful) => {
+        if (eventful.class[1] === 'operator') {
+            operatorInArray = true;
+        }
+    })
 
     // if number
     if (e.target.classList.contains('number')) {
 
-        console.log(lastEvent);
+        // obtain number
+        let number = e.target.innerText;
 
-        if (lastEvent === 'operator') {
-            console.log("the one before was operator");
+        // if events included operator, add to numberTwo
+        if (operatorInArray === true) {
+            numberTwo.push(number);
+            let numTwo = numberTwo.join('');
+            displayEl.textContent = `${numTwo}`;
+        } 
+
+        // if no event before was operator, add to numberOne
+        else {
+            numberOne.push(number);
+            let numOne = numberOne.join('');
+            displayEl.textContent = `${numOne}`;
         }
 
-        // Log number
-        let number = e.target.innerText;
-        numberOne.push(number);
-
-
+        console.log("Number 1 is: ", numberOne);
+        console.log("Number 2 is: ", numberTwo);
     }
 
     if (e.target.classList.contains('equals')){
-        // Show result
+        
+        let numOne = numberOne.join('');
+        let numTwo = numberTwo.join('');
 
+        // Show result based on the operation
+        if (functionToPerform === "subtraction") {
+            numberResult = subtract(numOne, numTwo);
+        } else if (functionToPerform === "addition") {
+            numberResult = addition(numOne, numTwo);
+        } else if (functionToPerform === "multiplication") {
+            numberResult = multiply(numOne, numTwo);
+        } else if (functionToPerform === "division") {
+            numberResult = division(numOne, numTwo);
+        } else {
+            numberResult = 0;
+        }
+        
+        // round the result to 2 decimals
+        numberResult = Math.round(numberResult * 100) / 100;
+        displayEl.textContent = `${numberResult}`;
+
+        // clear
+        functionToPerform = undefined;
+        events.length = 0;
+        numberOne.length = 0;
+        numberTwo.length = 0;
     }
 
     if (e.target.innerText === "-"){
         // Subtraction
-
+        functionToPerform = "subtraction";
     }
 
     if (e.target.innerText === "+"){
         // Addition
-
+        functionToPerform = "addition";
     }
 
     if (e.target.innerText === "/"){
         // Division
+        functionToPerform = "division";
     }
 
     if (e.target.innerText === "*"){
         // Multiplication
+        functionToPerform = "multiplication";
     }
 
     if (e.target.innerText === "C"){
         // Clear
+        functionToPerform = undefined;
+        events.length = 0;
+        numberOne.length = 0;
+        numberTwo.length = 0;
+        displayEl.textContent = `0`;
     }
 });
 
 /*-------------------------------- Functions --------------------------------*/
 
-// select number keys
-const numberSelector = (e) => {
-
-}
-
 function recordEvents(timeStamp, keyPressed, nameOfClass) {
     events.push({timestamp: timeStamp, key: keyPressed, class: nameOfClass});
-    console.log(events);
+}
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function addition(a, b) {
+    return (+a + +b);
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    return a / b;
 }
