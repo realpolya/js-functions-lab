b2223bc29f188433f88f3dbae06b4f06f798a6c3
realpolya <ps2780@columbia.edu>
2024-10-14 16:35:57 -0400

    start controlled forms lab

---
+++ unit3_labs/controlled-forms-lab/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className='main-page'>
+        <h1>Hello</h1>
+      </div>
+    </>
+  )
+
+}
+
+export default App
