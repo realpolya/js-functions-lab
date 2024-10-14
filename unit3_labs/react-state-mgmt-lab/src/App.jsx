7ba7205bf11c31412397f02603788bef94477946
realpolya <ps2780@columbia.edu>
2024-10-14 14:27:16 -0400

    work on react state lab

--- unit3_labs/react-state-mgmt-lab/src/App.jsx
+++ unit3_labs/react-state-mgmt-lab/src/App.jsx
@@ -1,9 +1,103 @@
 import './App.css'
+import { useState } from 'react';
 
 function App() {
+
+  const [team, setTeam] = useState([]);
+  const [money, setMoney] = useState(100);
+  const [fighters, setFighters] = useState([
+    {
+      name: 'Survivor',
+      price: 12,
+      strength: 6,
+      agility: 4,
+      img: 'https://images.unsplash.com/photo-1631913290783-490324506193?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      name: 'Scavenger',
+      price: 10,
+      strength: 5,
+      agility: 5,
+      img: 'https://images.unsplash.com/photo-1624213111452-35e8d3d5cc18?q=80&w=1452&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      name: 'Shadow',
+      price: 18,
+      strength: 7,
+      agility: 8,
+      img: 'https://via.placeholder.com/150/24f355',
+    },
+    {
+      name: 'Tracker',
+      price: 14,
+      strength: 7,
+      agility: 6,
+      img: 'https://via.placeholder.com/150/d32776',
+    },
+    {
+      name: 'Sharpshooter',
+      price: 20,
+      strength: 6,
+      agility: 8,
+      img: 'https://via.placeholder.com/150/1ee8a4',
+    },
+    {
+      name: 'Medic',
+      price: 15,
+      strength: 5,
+      agility: 7,
+      img: 'https://via.placeholder.com/150/66b7d2',
+    },
+    {
+      name: 'Engineer',
+      price: 16,
+      strength: 6,
+      agility: 5,
+      img: 'https://via.placeholder.com/150/56acb2',
+    },
+    {
+      name: 'Brawler',
+      price: 11,
+      strength: 8,
+      agility: 3,
+      img: 'https://via.placeholder.com/150/8985dc',
+    },
+    {
+      name: 'Infiltrator',
+      price: 17,
+      strength: 5,
+      agility: 9,
+      img: 'https://via.placeholder.com/150/392537',
+    },
+    {
+      name: 'Leader',
+      price: 22,
+      strength: 7,
+      agility: 6,
+      img: 'https://via.placeholder.com/150/602b9e',
+    },
+  ])
+
   return (
-    <h1>Hello world!</h1>
+    <>
+      <div className='main-page'>
+        <h1>Zombies in Reactville!</h1>
+        <div className='fighter-cards'>
+            {fighters.map((fighter, i) => {
+              console.log(fighter.name);
+              return <ul key={i} className='fighter-card'>
+                <li>Name: {fighter.name}</li>
+                <li>Price: ${fighter.price}</li>
+                <li>Strength: {fighter.strength}</li>
+                <li>Agility: {fighter.agility}</li>
+                <li><img src={fighter.img} /></li>
+              </ul>
+            })}
+        </div>
+      </div>
+    </>
   );
+
 }
 
 export default App
